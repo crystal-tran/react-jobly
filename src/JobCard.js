7ede1dbb5034301f71b1dfe62b9c28e94a2df538
Crystal Tran <c.t.tran95@gmail.com>
2024-03-09 12:54:38 -0500

    Updated job card UI

--- src/JobCard.js
+++ src/JobCard.js
@@ -17,12 +17,12 @@ function JobCard({ jobData }) {
   // console.log("JobCard", "jobData= ", jobData);
   const {title, companyName, salary, equity} = jobData
   return (
-    <div className="JobCard container">
+    <div className="JobCard container col-10 mb-3">
       <div className='card-body'>
         <h5><b>{title}</b></h5>
         <h7><i>{companyName}</i></h7>
-        <p className='mb-0 mt-3'>Salary: {salary}</p>
-        <p>Equity: {equity}</p>
+        <p className='mb-0 mt-2'>Salary: {salary}</p>
+        <p className='mb-0'>Equity: {equity}</p>
       </div>
     </div>
   );
